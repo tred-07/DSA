16e26f0ff98029f6b381b9c7d8f08efcbb9890d6
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-16 16:39:24 +0600

    Binary_Saerch

---
+++ 1_Binary_Search.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool binary_search(int a[],int n,int key){
+    int l=0,r=n;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(a[mid]==key)return 1;
+        if(a[mid]>key)r=mid-1;
+        else l=mid+1;
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cin>>n;
+    int key;
+    cin>>key;
+  int a[n];
+  for(int i=0;i<n;i++){
+    a[i]=i+1;
+    cout<<a[i]<<" ";
+    }
+    cout<<endl;
+  if(binary_search(a,n,key))cout<<"Find value"<<endl;
+  else cout<<"Not find value"<<endl;
+}
